43a3ffd85f1a4c08892f5e564ba3bef3550090f4
Teuscher Jonas, B2B-BAN-ABP-CEN-CA1 <Jonas.Teuscher@swisscom.com>
2024-05-23 16:41:46 -0400

    Add title

--- php/detail.php
+++ php/detail.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AAREWÄRT</title>
+    <title>AAREWÄRT - Detailahsicht</title>
 
     <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
